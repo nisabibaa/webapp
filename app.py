7c943b218aa32105863af73e93e11fa143719c72
nisabibaa <130288194+nisabibaa@users.noreply.github.com>
2023-12-08 21:48:01 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ list_doctor = ['', 'dr. Nurita', 'dr. Yogi', 'dr. Wibowo', 'dr. Ulama', 'dr. Pin
 list_symptom = ['', 'male', 'female']
 
 conn = st.connection("postgresql", type="sql", 
-                     url="postgresql://reza.habibi14:a1qzLyHmI9OQ@ep-long-wildflower-08546844.us-east-2.aws.neon.tech/web")
+                     url="postgresql://nisaabiba06:h7IagBqlpK9j@ep-noisy-fire-67397651.us-east-2.aws.neon.tech/web")
 with conn.session as session:
     query = text('CREATE TABLE IF NOT EXISTS SCHEDULE (id serial, doctor_name varchar, patient_name varchar, gender char(25), \
                                                        symptom text, handphone varchar, address text, tanggal date);')
