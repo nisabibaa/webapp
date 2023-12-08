01494b88a8967412ff2f652dd66751279e105580
nisabibaa <130288194+nisabibaa@users.noreply.github.com>
2023-12-08 21:50:34 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,71 +1,110 @@
 import streamlit as st
+import pandas as pd
 from sqlalchemy import text
 
-list_doctor = ['', 'dr. Nurita', 'dr. Yogi', 'dr. Wibowo', 'dr. Ulama', 'dr. Ping']
-list_symptom = ['', 'male', 'female']
+list_programs = ['', 'General English', 'Intensive IELTS', 'TOEFL Preparation', 'Business English', 'Conversational English']
+list_gender = ['', 'male', 'female']
+list_duration = ['', '2 months', '3 months']
+list_job = ['', 'Student', 'Worker', 'College Student']
+list_price = ['', 'Rp 2.000.000', 'Rp 2.500.000', 'Rp 3.000.000', 'Rp 3.500.000']
 
 conn = st.connection("postgresql", type="sql", 
                      url="postgresql://nisaabiba06:h7IagBqlpK9j@ep-noisy-fire-67397651.us-east-2.aws.neon.tech/web")
+
 with conn.session as session:
-    query = text('CREATE TABLE IF NOT EXISTS SCHEDULE (id serial, doctor_name varchar, patient_name varchar, gender char(25), \
-                                                       symptom text, handphone varchar, address text, tanggal date);')
+    query = text('CREATE TABLE IF NOT EXISTS PARTICIPANT (id serial, full_name varchar, gender varchar, birth date, \
+                                                       ages int, city varchar, job varchar, institution varchar, \
+                                                       email varchar, handphone varchar, programs varchar, \
+                                                       duration varchar, price varchar, starting_date date, ending_date date);')
     session.execute(query)
 
-st.header('SIMPLE HOSPITAL DATA MANAGEMENT SYS')
-page = st.sidebar.selectbox("Pilih Menu", ["View Data","Edit Data"])
+st.header('DATABASE PARTICIPANT OF KAMPUNG INGGRIS 2023')
+page = st.sidebar.selectbox("Pilih Menu", ["View Data", "Edit Data", "Visualisasi Data"])
 
 if page == "View Data":
-    data = conn.query('SELECT * FROM schedule ORDER By id;', ttl="0").set_index('id')
+    data = conn.query('SELECT * FROM PARTICIPANT ORDER By id;', ttl="0").set_index('id')
     st.dataframe(data)
 
 if page == "Edit Data":
     if st.button('Tambah Data'):
         with conn.session as session:
-            query = text('INSERT INTO schedule (doctor_name, patient_name, gender, symptom, handphone, address, waktu, tanggal) \
-                          VALUES (:1, :2, :3, :4, :5, :6, :7, :8);')
-            session.execute(query, {'1':'', '2':'', '3':'', '4':'[]', '5':'', '6':'', '7':None, '8':None})
+            query = text('INSERT INTO PARTICIPANT (full_name, gender, birth, ages, city, job, institution, email, handphone, \
+                                                    programs, duration, price, starting_date, ending_date) \
+                          VALUES (:1, :2, :3, :4, :5, :6, :7, :8, :9, :10, :11, :12, :13, :14);')
+            session.execute(query, {'1': '', '2': '', '3': None, '4': 0, '5': '', '6': '', '7': '', '8': '', '9': '',
+                                    '10': '', '11': '', '12': 0, '13': None, '14': None})
             session.commit()
 
-    data = conn.query('SELECT * FROM schedule ORDER By id;', ttl="0")
-    for _, result in data.iterrows():        
+    data = conn.query('SELECT * FROM PARTICIPANT ORDER By id;', ttl="0")
+    for _, result in data.iterrows():
         id = result['id']
-        doctor_name_lama = result["doctor_name"]
-        patient_name_lama = result["patient_name"]
+        full_name_lama = result["full_name"]
         gender_lama = result["gender"]
-        symptom_lama = result["symptom"]
+        birth_lama = result["birth"]
+        ages_lama = result["ages"]
+        city_lama = result["city"]
+        job_lama = result["job"]
+        institution_lama = result["institution"]
+        email_lama = result["email"]
         handphone_lama = result["handphone"]
-        address_lama = result["address"]
-        waktu_lama = result["waktu"]
-        tanggal_lama = result["tanggal"]
+        programs_lama = result["programs"]
+        duration_lama = result["duration"]
+        price_lama = result["price"]
+        starting_date_lama = result["starting_date"]
+        ending_date_lama = result["ending_date"]
 
-        with st.expander(f'a.n. {patient_name_lama}'):
+        with st.expander(f'a.n. {full_name_lama}'):
             with st.form(f'data-{id}'):
-                doctor_name_baru = st.selectbox("doctor_name", list_doctor, list_doctor.index(doctor_name_lama))
-                patient_name_baru = st.text_input("patient_name", patient_name_lama)
-                gender_baru = st.selectbox("gender", list_symptom, list_symptom.index(gender_lama))
-                symptom_baru = st.multiselect("symptom", ['cough', 'flu', 'headache', 'stomache'], eval(symptom_lama))
+                full_name_baru = st.text_input("full_name", full_name_lama)
+                gender_baru = st.selectbox("gender", list_gender, list_gender.index(gender_lama) if gender_lama in list_gender else 0)
+                birth_baru = st.date_input("birth", birth_lama)
+                ages_baru = st.number_input("ages", value=ages_lama)
+                city_baru = st.text_input("city", city_lama)
+                job_baru = st.selectbox("job", list_job, list_job.index(job_lama) if job_lama in list_job else 0)
+                institution_baru = st.text_input("institution", institution_lama)
+                email_baru = st.text_input("email", email_lama)
                 handphone_baru = st.text_input("handphone", handphone_lama)
-                address_baru = st.text_input("address", address_lama)
-                waktu_baru = st.time_input("waktu", waktu_lama)
-                tanggal_baru = st.date_input("tanggal", tanggal_lama)
-                
+                programs_baru = st.selectbox("programs", list_programs, list_programs.index(programs_lama) if programs_lama in list_programs else 0)
+                duration_baru = st.selectbox("duration", list_duration, list_duration.index(duration_lama) if duration_lama in list_duration else 0)
+                price_baru = st.selectbox("price", list_price, list_price.index(price_lama) if price_lama in list_price else 0)
+                starting_date_baru = st.date_input("starting_date", starting_date_lama)
+                ending_date_baru = st.date_input("ending_date", ending_date_lama)
+
                 col1, col2 = st.columns([1, 6])
 
                 with col1:
                     if st.form_submit_button('UPDATE'):
                         with conn.session as session:
-                            query = text('UPDATE schedule \
-                                          SET doctor_name=:1, patient_name=:2, gender=:3, symptom=:4, \
-                                          handphone=:5, address=:6, waktu=:7, tanggal=:8 \
-                                          WHERE id=:9;')
-                            session.execute(query, {'1':doctor_name_baru, '2':patient_name_baru, '3':gender_baru, '4':str(symptom_baru), 
-                                                    '5':handphone_baru, '6':address_baru, '7':waktu_baru, '8':tanggal_baru, '9':id})
+                            query = text('UPDATE PARTICIPANT \
+                                          SET full_name=:1, gender=:2, birth=:3, ages=:4, city=:5, job=:6, institution=:7, \
+                                          email=:8, handphone=:9, programs=:10, duration=:11, price=:12, \
+                                          starting_date=:13, ending_date=:14 \
+                                          WHERE id=:15;')
+                            session.execute(query, {'1': full_name_baru, '2': gender_baru, '3': birth_baru,
+                                                    '4': ages_baru, '5': city_baru, '6': job_baru, '7': institution_baru,
+                                                    '8': email_baru, '9': handphone_baru, '10': programs_baru,
+                                                    '11': duration_baru, '12': price_baru, '13': starting_date_baru,
+                                                    '14': ending_date_baru, '15': id})
                             session.commit()
                             st.experimental_rerun()
-                
+
                 with col2:
                     if st.form_submit_button('DELETE'):
-                        query = text(f'DELETE FROM schedule WHERE id=:1;')
-                        session.execute(query, {'1':id})
+                        query = text(f'DELETE FROM PARTICIPANT WHERE id=:1;')
+                        session.execute(query, {'1': id})
                         session.commit()
                         st.experimental_rerun()
+
+
+if page == "Visualisasi Data":
+    st.subheader("Visualisasi Programs")
+    data = conn.query('SELECT programs, COUNT(*) as count FROM participant GROUP BY programs;')
+    st.bar_chart(data.set_index('programs'))
+    '\n'
+    st.subheader("Visualisasi Duration")
+    data = conn.query('SELECT duration, COUNT(*) as count FROM participant GROUP BY duration;')
+    st.bar_chart(data.set_index('duration'))
+    '\n'
+    st.subheader("Visualisasi Price")
+    data = conn.query('SELECT price, COUNT(*) as count FROM participant GROUP BY price;')
+    st.bar_chart(data.set_index('price'))
