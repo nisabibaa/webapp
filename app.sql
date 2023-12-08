48ca9f0ddb77045567107bb7ea3da45ecfa58c98
nisabibaa <130288194+nisabibaa@users.noreply.github.com>
2023-12-08 21:51:06 +0700

    Update app.sql

--- app.sql
+++ app.sql
@@ -1,26 +1,31 @@
-drop table if exists schedule;
-create table schedule (
+drop table if exists participant;
+create table participant (
 	id serial,
-	doctor_name text,
-	patient_name text,
+	full_name text,
 	gender text,
-	symptom text,
+	birth date,
+	ages int,
+	city text,
+	job text,
+	institution text,
+	email text,
 	handphone text,
-	address text,
-	waktu time,
-	tanggal date
+	programs text,
+	duration text,
+	price text,
+	starting_date date,
+	ending_date date
 );
 
-insert into schedule (doctor_name, patient_name, gender, symptom, handphone, address, waktu, tanggal) 
+insert into participant (full_name, gender, birth, ages, city, job, institution, email, handphone, programs, duration, price, starting_date, ending_date)
 values
-	('dr. Nurita', 'Ahmad Maulana', 'male', '["headache", "stomache"]', 62838, 'address1', '08:00', '2023-10-01'),
-	('dr. Yogi', 'Renata Zahab', 'female', '["cough", "flu"]', 62838, 'address2', '09:00', '2022-10-02'),
-	('dr. Wibowo', 'Nunuk Reni', 'female', '["cough", "flu"]', 62838, 'address3', '10:00', '2022-10-03'),
-	('dr. Yogi', 'Bro Ulil', 'male', '["cough", "headache"]', 62838, 'address4', '11:00', '2022-10-04'),
-	('dr. Ulama', 'Wah Bowi', 'male', '["headache", "flu"]', 62838, 'address5', '12:00', '2022-10-05'),
-	('dr. Ulama', 'Iis Mika', 'female', '["cough", "flu", "stomache", "headache"]', 62838, 'address6', '08:00', '2022-10-06'),
-	('dr. Ping', 'Zizah Lana', 'female', '["flu", "stomache", "headache"]', 62838, 'address7', '09:00', '2022-10-07'),
-	('dr. Nurita', 'Alif Iman', 'male', '["cough", "flu", "headache"]', 62838, 'address8', '10:00', '2022-10-08'),
-	('dr. Ping', 'Zaka Zaki', 'female', '["cough", "stomache", "headache"]', 62838, 'address9', '11:00', '2022-10-09'),
-	('dr. Wibowo', 'Faus Rahmi', 'male', '["cough"]', 62838, 'address10', '12:00', '2022-10-11')
-	;
+  ('Jerome Polin', 'male', '2008-01-01', 15, 'Surabaya', 'Student', 'SMAN 5 Surabaya', 'jeromepolin@gmail.com', '6283812345876', 'General English', '2 months', 2000000, '2023-10-01', '2023-11-30'),
+  ('Maudy Ayunda', 'female', '2007-12-31', 16, 'Jombang', 'Student', 'SMAN 2 Jombang', 'maudyayunda@gmail.com', '6283896329864', 'Intensive IELTS', '3 months', 3000000, '2023-09-01', '2023-11-30'),
+  ('Tiara Andini', 'female', '2006-11-30', 17, 'Bekasi', 'Student', 'SMAN 1 Bekasi', 'tiaraandini@gmail.com', '6283886234109', 'TOEFL Preparation', '2 months', 2500000, '2023-10-01', '2023-11-30'),
+  ('Jefri Nichol', 'male', '2005-10-31', 18, 'Banyuwangi', 'Worker', 'Point Coffee', 'jefrinichol@gmail.com', '628385316457', 'Business English', '3 months', 3500000, '2023-09-01', '2023-11-30'),
+  ('Iqbaal Ramadhan', 'male', '2004-09-30', 19, 'Pasuruan', 'College Student', 'Unair', 'iqbaalramadhan@gmail.com', '6283894583190', 'Conversational English', '2 months', 2000000, '2023-10-01', '2023-11-30'),
+  ('Syifa Hadju', 'female', '2003-08-31', 20, 'Gresik', 'College Student', 'Unika Atma Jaya', 'syifahadju@gmail.com', '6283820432211', 'General English', '3 months', 3000000, '2023-09-01', '2023-11-30'),
+  ('Ziva Magnolya', 'female', '2002-07-31', 21, 'Kediri', 'Worker', 'Transmart', 'zivamagnolya@gmail.com', '6283874820274', 'Intensive IELTS', '2 months', 2500000, '2023-10-01', '2023-11-30'),
+  ('Refal Hadi', 'male', '2001-06-30', 22, 'Sidoarjo', 'College Student', 'ITS', 'refalhadi@gmail.com', '6283873628496', 'TOEFL Preparation', '3 months', 3500000, '2023-09-01', '2023-11-30'),
+  ('Angga Yunanda', 'male', '2000-05-31', 23, 'Malang', 'College Student', 'UI', 'anggayunanda@gmail.com', '6283872947145', 'Business English', '2 months', 2000000, '2023-09-01', '2023-10-30')
+;
